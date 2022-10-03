2d1ba5747030db27de0c45dac408a73432f22549
Shahriazshuvo <Shahriaz.Montakim@brainstation-23.com>
2022-10-03 11:58:56 +0600

    named annotation

--- app/src/main/java/com/example/daggerexample/NotificationServiceModule.kt
+++ app/src/main/java/com/example/daggerexample/NotificationServiceModule.kt
@@ -2,12 +2,20 @@ package com.example.daggerexample
 
 import dagger.Module
 import dagger.Provides
+import javax.inject.Named
 
 @Module
 class NotificationServiceModule {
 
+    @MessageQualifier
     @Provides
     fun getMessageService():NotificationService{
         return MessageService()
+    }
+
+    @Named("email")
+    @Provides
+    fun getEmailService(emailService: EmailService):NotificationService{
+        return emailService
     }
 }
