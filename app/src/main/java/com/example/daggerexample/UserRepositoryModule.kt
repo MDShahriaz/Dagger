f2458ee0a2afb65446340951de17731797607a03
Shahriazshuvo <Shahriaz.Montakim@brainstation-23.com>
2022-10-03 11:44:20 +0600

    binds annotation

--- app/src/main/java/com/example/daggerexample/UserRepositoryModule.kt
+++ app/src/main/java/com/example/daggerexample/UserRepositoryModule.kt
@@ -1,13 +1,12 @@
 package com.example.daggerexample
 
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
 
 @Module
-class UserRepositoryModule(){
+abstract class UserRepositoryModule(){
 
-    @Provides
-    fun getFirebaseRepository():UserRepository{
-        return FirebaseRepository()
-    }
+    @Binds
+    abstract fun getSQLRepository(sqlRepository: SQLRepository):UserRepository
 }
