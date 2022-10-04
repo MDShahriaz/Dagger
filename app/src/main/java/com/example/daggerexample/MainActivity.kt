2e62ed303234ab85de3dae86e74ca1a900832485
Shahriazshuvo <Shahriaz.Montakim@brainstation-23.com>
2022-10-04 13:41:06 +0600

    new work

--- app/src/main/java/com/example/daggerexample/MainActivity.kt
+++ app/src/main/java/com/example/daggerexample/MainActivity.kt
@@ -15,7 +15,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         val appComponent = (application as UserApplication).appComponent
         val userRegistrationComponent = appComponent.getUserRegistrationComponentFactory().create(3)
         userRegistrationComponent.inject(this)
